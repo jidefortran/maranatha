b86bc86b96e45d2b1342478eaa5cf6e2bd057071
Jide Fortran <gamegrafix@yahoo.com>
2023-09-21 20:53:48 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import Navbar from "../components/Layouts/Navbar";
 import MainBanner from "../components/HomeOne/MainBanner";
-import About from "../components/HomeOne/About";
+
 import Services from "../components/HomeOne/Services";
-import Webinar from "../components/HomeOne/Webinar";
-import PartnerContent from "../components/Common/PartnerContent";
-import FeedbackSlider from "../components/Common/FeedbackSlider";
-import PricingCard from "../components/Common/PricingCard";
+
 import BlogPost from "../components/Common/BlogPost";
-import FreeTrialForm from "../components/Common/FreeTrialForm";
+
 import Footer from "../components/Layouts/Footer";
 import WhyChooseUs from "../components/HomeOne/WhyChooseUs";
 
