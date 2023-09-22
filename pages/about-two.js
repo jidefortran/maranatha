aba8954dd81c26ee86598caab1198291a70c1c96
Jide Fortran <gamegrafix@yahoo.com>
2023-09-21 23:24:41 -0400

    rerih'jl;

--- pages/about-two.js
+++ pages/about-two.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../components/Layouts/Navbar";
-import Footer from "../components/Layouts/Footer"
+import Footer from "../components/Layouts/Footer";
 import PageBanner from "../components/Common/PageBanner";
 import AboutUsContent from "../components/AboutTwo/AboutUsContent";
 import ProfessionalSolutions from "../components/HomeFive/ProfessionalSolutions";
