3c23c81ba8b5dedb19f433a5f875fde451b6fdb3
Jide Fortran <gamegrafix@yahoo.com>
2023-09-30 13:58:47 -0400

    image fix

--- components/FeaturedImage.js
+++ components/FeaturedImage.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import defImage from "../styles/images/dancer.jpg"
 
 export default function FeaturedImage({ post }) {
     let img = '';
 
-    const defaultFeaturedImage = "https://wp.abhinavr.com/wp-content/uploads/2022/12/travel_icy-polar_022K.jpg";
+    const defaultFeaturedImage = defImage;
     const defaultWidth = "300";
     const defaultHeight = "200";
 
     if(post.featuredImage) {
-        let size = post.featuredImage.node.mediaDetails.sizes[4];
+        let size = post.featuredImage.node.mediaDetails.sizes[0];
         img = {
             src: size.sourceUrl,
             width: size.width,
